ad8d43f97ff96d1828d4ddb04ef128560421431b
Alena <shining8smaragd@gmail.com>
2026-04-22 20:23:15 +0300

    Fix the code for submitted forData object

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -47,12 +47,18 @@ const onFeedbackFormSubmit = event => {
 
   localStorage.removeItem('feedback-form-state');
   refs.feedbackForm.reset();
+  
+// Ви це мали на увазі? 
+
+  formData = {
+    email: "",
+    message: "",
+  };   
+  
 };
 
 
 document.addEventListener('DOMContentLoaded', initFeedbackFormPage);
 refs.feedbackForm.addEventListener('input', onFeedbackFormFieldChange);
 refs.feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
-// Використовуй метод делегування для відстеження змін у формі через подію input. Зберігай актуальні дані з полів email та message у formData та записуй цей об’єкт у локальне сховище. Використовуй ключ "feedback-form-state" для зберігання даних у сховищі.
-// При завантаженні сторінки перевір, чи є дані у локальному сховищі. Якщо так, використовуй їх для заповнення форми та об'єкта formData. Якщо ні, залиш поля форми порожніми.
-// Перед відправленням форми переконайся, що обидва поля форми заповнені. Якщо будь-яке з полів (властивостей об’єкта formData) порожнє, показуй сповіщення з текстом «Fill please all fields». Якщо всі поля заповнені, виведи у консоль об’єкт formData з актуальними значеннями, очисти локальне сховище, об’єкт formData і поля форми.
+
